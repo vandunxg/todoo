d26a3f603640e9b384ea533921f4d8752ea5763b
vandunxg <nguyenvandung.official@gmail.com>
2025-01-19 23:23:05 +0700

    init project

---
+++ src/main/java/com/vandunxg/todoo/TodooApplication.java
@@ -0,0 +1,13 @@
+package com.vandunxg.todoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodooApplication.class, args);
+	}
+
+}
